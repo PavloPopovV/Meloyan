7f2667beaeb84197b1c92aed60d9332924679325
PavloPopovV <pavlopopov570@gmail.com>
2022-08-26 15:44:32 +0300

    please help

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 
 $(document).ready(function () {
-    
     $('.slider_wrapper').slick({
         arrows:true,
         infinity:true,
         variableWidth: true,
     });
 });
-
 
 $(document).ready(function () {
     var $list = $(".wrapper .mix").hide(),
@@ -41,7 +39,7 @@ $(function () {
     });
     $(".example").on("click", function () {
         $(".story_work_card").hide();
-        $(".lovestory_wrapper").hide();
+        $(".disable").hide();
         $(".wrapper").show();
     });
 });
@@ -53,23 +51,148 @@ $(function () {
     $('.lovestory_card').click(function () {
         $('body,html').animate({ scrollTop: 0 }, 100);
     })
+    $('.business_card').click(function () {
+        $('body,html').animate({ scrollTop: 0 }, 100);
+    })
+    
+});
+
+$(function () {
+    $('.individual').click(function (e) {
+        $('.portfolio').hide();
+        e.preventDefault();
+        $('.individual_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.individual_card').click(function (e) {
+        $('.individual_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
+    // ------------------------
     $('.lovestory').click(function (e) {
         $('.portfolio').hide();
         e.preventDefault();
         $('.lovestory_wrapper').fadeToggle('slow');
         e.preventDefault();
     })
-});
-
-$(function () {
     $('.lovestory_card').click(function (e) {
         $('.lovestory_wrapper').hide();
         e.preventDefault();
         $('.story_work_card').fadeToggle('slow');
         e.preventDefault();
     })
+    // ----------------------------
+    $('.family').click(function (e) {
+        $('.portfolio').hide();
+        e.preventDefault();
+        $('.family_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.family_card').click(function (e) {
+        $('.family_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
+    // -----------------------------
+    $('.prag').click(function (e) {
+        $('.portfolio').hide();
+        e.preventDefault();
+        $('.prag_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.prag_card').click(function (e) {
+        $('.prag_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
+    // ----------------------------------
+    $('.newYear').click(function (e) {
+        $('.portfolio').hide();
+        e.preventDefault();
+        $('.newYear_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.newYear_card').click(function (e) {
+        $('.newYear_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
+    // ------------------------------------
+    $('.projects').click(function (e) {
+        $('.portfolio').hide();
+        e.preventDefault();
+        $('.projects_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.projects_card').click(function (e) {
+        $('.projects_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
+    // ----------------------------
+    $('.food').click(function (e) {
+        $('.business').hide();
+        e.preventDefault();
+        $('.food_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.food_card').click(function (e) {
+        $('.food_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
+    // -----------------------------------
+    $('.catalog').click(function (e) {
+        $('.business').hide();
+        e.preventDefault();
+        $('.catalog_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.catalog_card').click(function (e) {
+        $('.catalog_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
+    // -----------------------------------
+    $('.recording').click(function (e) {
+        $('.business').hide();
+        e.preventDefault();
+        $('.recording_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.recording_card').click(function (e) {
+        $('.recording_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
+    // -----------------------------------
+    $('.fitnes').click(function (e) {
+        $('.business').hide();
+        e.preventDefault();
+        $('.fitnes_wrapper').fadeToggle('slow');
+        e.preventDefault();
+    })
+    $('.fitnes_card').click(function (e) {
+        $('.fitnes_wrapper').hide();
+        e.preventDefault();
+        $('.story_work_card').fadeToggle('slow');
+        e.preventDefault();
+    })
 });
 
+$(function () {
+    
+   
+});
 
 
 
